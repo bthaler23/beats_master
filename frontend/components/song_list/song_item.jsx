388a000658931040285909d7e370e1eea4dd0739
Brandon Thaler <bthaler@bellsouth.net>
2017-04-19 12:30:10 -0400

    Update styling for SongListItem

--- frontend/components/song_list/song_item.jsx
+++ frontend/components/song_list/song_item.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 
 const SongItem = (props) => {
-  debugger
   return (
-    <li>{props.song.title}</li>
+    <li className="song_item">
+      <img src={props.song.image_url}/>
+      <div className="song_header">
+          <img src={window.playButton}/>
+        <div className="song_header_info">
+          <div>{props.song.artist}</div>
+          <div>{props.song.title}</div>
+        </div>
+      </div>
+    </li>
   )
 
 }
